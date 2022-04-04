933e4685aa39cfbbc254899ca877b064281475a8
nczhu <n@pragmatic.vc>
2022-04-04 12:15:51 +0100

    test: location and biome ids passing

--- circuits/test/list.js
+++ circuits/test/list.js
@@ -7,21 +7,20 @@ const Scalar = require("ffjavascript").Scalar;
 exports.p = Scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495617");
 const Fr = new F1Field(exports.p);
 
-// what is this... 
 const wasm_tester = require("circom_tester").wasm;
 
 describe("List coordinates test", function () {
 
 	this.timeout(100000);
 
-	it("Should create a list circuit", async () => {
+	it("Should list correctly", async () => {
 		const circuit = await wasm_tester(path.join(__dirname, "..", "market", "list.circom"));
 
 		let witness;
 		witness = await circuit.calculateWitness(
 			{
 				"PLANETHASH_KEY": 7,
-				"BIOMEBASE_KEY": 7,
+				"BIOMEBASE_KEY": 8,
 				"SCALE": 4096,
 				"xMirror": 0,
 				"yMirror": 0,
@@ -29,11 +28,15 @@ describe("List coordinates test", function () {
 				"y": "21888242871839275222246405745257275088548364400416034343698204186575808492485",
 				"key": 1
 			}, true);
-		await circuit.assertOut(witness, { listing_id: 5 });
+
+		// Checked against output.json from https://github.com/darkforest-eth/circuits
+		await circuit.assertOut(witness, { biomebase: 12 });
+		await circuit.assertOut(witness, { planet_id: Fr.e("15744909102780347355599901106611655633588302959081107425005702788497286612323") });
+
+		// TODO: implement encryption
 		await circuit.assertOut(witness, { key_commitment: 4 });
-		await circuit.assertOut(witness, { biomebase: 14 });
-		// TODO print out wtf is FR.e...
-		// TODO generate witness in the darkforest impl, compare with my witness
+		await circuit.assertOut(witness, { listing_id: 5 });
+
 		// assert(Fr.eq(Fr.e(witness[1]), Fr.e(1)));
 	});
 });
