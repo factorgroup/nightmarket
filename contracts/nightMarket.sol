427524beb5c095d3c7956c1e6388364dfc1bfbb6
nczhu <n@pragmatic.vc>
2022-04-11 22:37:46 +0000

    chore: scaffoled rest of contract

--- contracts/nightMarket.sol
+++ contracts/nightMarket.sol
@@ -41,27 +41,28 @@ contract NightMarket is ReentrancyGuard {
 
     //  Type
     struct Order {
-        // Address buyer , now used as key, TODO: check potential bugs on this...
+        address payable buyer;
         uint expectedSharedKeyHash;
-        uint64 created;                     // when order was created
+        uint created;                       // block timestamp when order was created
         bool isActive;                      // inactive after refund/sale
     }
 
     struct Listing {
         address payable seller;
-        uint locationId;
-        uint biomebase;
+        uint locationId;         // prob doesn't need to be stored, just emitted
+        uint biomebase;          // prob doesn't need to be stored, just emitted
         uint keyCommitment;                 // H(key) being sold, can't store in events?
-        uint nonce;
+        uint nonce;             // prob doesn't need to be stored, just emitted
         uint price;                         // cost of key to coordinates
         uint64 escrowTime;                  // time buyer's deposit is locked up for, use timers.sol
-        bool isActive;                      // depends on if we allow sellers to delist 
-        mapping (uint => Order ) orders;    // uint is just buyer address, only 1 active buy/lsiting at atime, Make it public?
+        bool isActive;                      // depends on if we allow sellers to delist
+        uint numOrders;
+        mapping (uint => Order ) orders;    // key starts at numOrders=1
     }
 
     // States
-    uint public numListings;                       // incremental, maybe use Counters.sol
-    mapping (uint => Listing) public listings;     // uint is incremental; make this public?
+    uint public numListings;                       // TODO: maybe use Counters.sol for safer math
+    mapping (uint => Listing) public listings;     // Key starts at numListings=1
 
     // DF storage contract
     GetterInterface df;
@@ -74,10 +75,10 @@ contract NightMarket is ReentrancyGuard {
     SaleVerifier public saleVerifier;
 
     // Events
-    event Listed();
-    event Delisted();
+    event Listed(address indexed seller, uint indexed listingId);
+    event Delisted(address indexed seller, uint indexed listingId);
     event Asked();
-    event Sold();
+    event Sold(uint nonce);
     event Refunded();
 
     // TODO write test for this
@@ -99,25 +100,23 @@ contract NightMarket is ReentrancyGuard {
         SaleVerifier _saleVerifier,
         address _dfGetterContract
     ) {
-        // Initialize with verifier contracts
         listVerifier = _listVerifier;
         saleVerifier = _saleVerifier;
         
         //TODO(later): initialize w/ diamond address rather than DFGetterFacet contract?
         df = GetterInterface(_dfGetterContract);
-        
         gameConstants = df.getSnarkConstants();
     }
 
     /**
     * @notice Seller can list a secret Dark Forest coordinate for sale
-    * @dev `proof` is a zkSnarks proof defined in `list.circom`
+    * @dev Seller generates `_proof` offchain in `list.circom`
     * @param _proof The listing_id proof
     * @return listingId as in listings[listingId] => Listing 
     */
     function list(
         uint[8] memory _proof,
-        uint[4] memory _listingId,
+        uint[4] memory _coordEncryption,
         uint _nonce,
         uint _keyCommitment,
         uint _locationId,
@@ -139,10 +138,10 @@ contract NightMarket is ReentrancyGuard {
             gameConstants.PERLIN_LENGTH_SCALE,
             boolToUInt(gameConstants.PERLIN_MIRROR_X),
             boolToUInt(gameConstants.PERLIN_MIRROR_Y),
-            _listingId[0],
-            _listingId[1],
-            _listingId[2],
-            _listingId[3],
+            _coordEncryption[0],
+            _coordEncryption[1],
+            _coordEncryption[2],
+            _coordEncryption[3],
             _nonce,
             _keyCommitment,
             _locationId,
@@ -155,7 +154,9 @@ contract NightMarket is ReentrancyGuard {
             "Seller list coordinates: invalid proof"
         );
 
-        Listing storage l = listings[numListings];
+        listingId = numListings++;
+
+        Listing storage l = listings[listingId];
         l.seller = payable(msg.sender);
         l.locationId = _locationId;
         l.biomebase = _biomebase;
@@ -165,35 +166,92 @@ contract NightMarket is ReentrancyGuard {
         l.escrowTime = _escrowTime;
         l.isActive = true;
 
-        numListings++;
+        emit Listed(msg.sender, numListings); // Log more things later as needed
+    }
 
-        emit Listed();
+    /**
+    * @notice Seller can delist an active listing
+    * @dev Sellers who care about reputation will use this fn, otherwise, unlikely
+    * @param _listingId the ID from list() step
+    */
+    function delist(uint _listingId) external {
+        Listing storage l = listings[_listingId];
+        require(msg.sender == l.seller);
+        l.isActive = false;
+        emit Delisted(msg.sender, numListings);
+    }
+
+    /**
+    * @notice Buyer can ask on active listings
+    * @dev 
+    * @param _expectedSharedKeyHash Buyer computes H(ecdh(buyerPrivKey, sellerPubKey))
+    * return orderId Buyer keeps this for future refunds
+    */
+    function ask(uint _listingId, uint _expectedSharedKeyHash) external payable returns (uint orderId) {       //noreentrance? payable?
+        Listing storage l = listings[_listingId];
+        require(l.isActive, "Listing is no longer active");
+        require(msg.value == l.price);
         
-        return(numListings-1);
+        l.orders[l.numOrders++] = Order({
+            buyer: payable(msg.sender),
+            expectedSharedKeyHash: _expectedSharedKeyHash,
+            created: block.timestamp,
+            isActive: true
+        });
+        return l.numOrders;
     }
 
     /**
-    * TODO
+    * @notice Seller can submit a proof of sale
+    * @dev Seller generates `_proof` offchain in `sale.circom`
     */
-    function delist() external {                     // good for seller to build reputation i guess
+    function sale(
+        uint _listingId,
+        uint _orderId,
+        uint[8] memory _proof,
+        uint[4] memory _keyEncryption,
+        uint _nonce
+    ) external {              // norenentrance?
+        Listing storage l = listings[_listingId];
+        require(l.seller == msg.sender);
+        require(l.isActive);
+        require(_nonce < 2^218);
 
+        // TODO: double check can buyer submit valid proof on different order?
+        // TODO: review any other commits to be checked
+        Order memory o = l.orders[_orderId];
+        
+        uint256[9] memory publicInputs = [
+            // User input or retrieve at contract level? doesn't matter bc we check H(sharedkey) anyway
+            // TODO: how to get buyer xy coordinates?
+            0,
+            0,
+            _keyEncryption[0],
+            _keyEncryption[1],
+            _keyEncryption[2],
+            _keyEncryption[3],
+            _nonce,
+            l.keyCommitment,
+            o.expectedSharedKeyHash
+        ];
+
+        require(saleVerifier.verify(_proof, publicInputs), "sale proof invalid");
+
+        // TODO Pay the seller, withdraw() pattern
+
+        l.orders[_orderId].isActive = false;
     }
 
     /**
+    * @notice Anyone can request a refund on a qualifying order
+    * @dev Seller can force refund (spam) buyers who submitted invalid expectedSharedKeyHash
+    * @param _listingId the ID from list() step
+    * @param _orderId the ID from ask() step
     */
-    function ask() external payable {       //noreentrance? payable?
-
-    }
-
-    /**
-    */
-    function sale() external {              //norenentrance?
-
-    }
-
-    /**
-    */
-    function refund() public {        //orderid, callable by anyone? or just buyer... becareful of contract buyers
-
+    function refund(uint _listingId, uint _orderId) public {        //orderid, callable by anyone? or just buyer... becareful of contract buyers
+        // Refund conditions:
+        // listing is inactive (seller closed) OR
+        // escrow time is past.
+        // Pay the buyer (be careful)
     }
 }
