773e6dc2fe31eb05f8fd1c289d5ece01c9999f97
nczhu <n@pragmatic.vc>
2022-04-11 16:35:56 +0000

    feat: scaffold interface around DFGetter contract

--- contracts/nightMarket.sol
+++ contracts/nightMarket.sol
@@ -2,8 +2,16 @@
 pragma solidity >=0.8.0;
 
 import "../github/OpenZeppelin/openzeppelin-contracts/contracts/security/ReentrancyGuard.sol";
+import {WithStorage, SnarkConstants, GameConstants} from "../github/darkforest-eth/eth/contracts/libraries/LibStorage.sol";
 import {Verifier as ListVerifier} from "./listVerifier.sol";
 import {Verifier as SaleVerifier} from "./saleVerifier.sol";
+import {GetterInterface} from "./GetterInterface.sol";
+
+function boolToUInt(bool x) pure returns (uint256 r) {
+    assembly {
+        r := x
+    }
+}
 
 /**
  * @title NightMarket
@@ -33,16 +41,15 @@ contract NightMarket is ReentrancyGuard {
         mapping(uint256 => Order) orders; // uint is just buyer address, only 1 active buy/lsiting at atime, Make it public?
     }
 
-    // Constants
-    uint256 constant PLANETHASH_KEY = uint256(7);
-    uint256 constant BIOMEBASE_KEY = uint256(8);
-    uint256 constant SCALE = uint256(4096);
-    uint256 constant xMirror = uint256(0); //double check is this user input?
-    uint256 constant yMirror = uint256(0);
+    // Game Constants
+    SnarkConstants gameConstants;
 
     // States
-    uint256 numListings; // incremental, maybe use Counters.sol
-    mapping(uint256 => Listing) listings; // uint is incremental; make this public?
+    uint256 public numListings; // incremental, maybe use Counters.sol
+    mapping(uint256 => Listing) public listings; // uint is incremental; make this public?
+
+    // DF storage contract
+    GetterInterface df;
 
     // Verifier functions
     ListVerifier public listVerifier;
@@ -58,15 +65,30 @@ contract NightMarket is ReentrancyGuard {
     // Checks planet hash is actually in the game
     modifier validPlanet(uint256 planetId) {
         // TODO
+        // GameStorage has:
+        // planetIds array, planets mapping
+        // check location not in revealedPlanetIds, revealedCoords mapping
         require(true);
         _;
     }
 
-    constructor(ListVerifier _listVerifier, SaleVerifier _saleVerifier) {
+    constructor(
+        ListVerifier _listVerifier,
+        SaleVerifier _saleVerifier,
+        address _dfGetterContract
+    ) {
+        // Reference the deployed verifier contracts
+
         listVerifier = _listVerifier;
         saleVerifier = _saleVerifier;
 
-        //TODO: initialize with DF game contract
+        //TODO(later): initialize w/ diamond address with additional checks
+        df = GetterInterface(_dfGetterContract);
+
+        // TODO: get this working with existing game contract
+        require(df.getSnarkConstants().PLANETHASH_KEY == 12345);
+
+        gameConstants = df.getSnarkConstants();
     }
 
     /**
@@ -92,12 +114,13 @@ contract NightMarket is ReentrancyGuard {
     {
         require(_nonce < 2 ^ 218);
 
-        uint256[14] memory publicInputs = [
-            PLANETHASH_KEY,
-            BIOMEBASE_KEY,
-            SCALE,
-            xMirror,
-            yMirror,
+        uint256[15] memory publicInputs = [
+            gameConstants.PLANETHASH_KEY,
+            gameConstants.BIOMEBASE_KEY,
+            gameConstants.SPACETYPE_KEY,
+            gameConstants.PERLIN_LENGTH_SCALE,
+            boolToUInt(gameConstants.PERLIN_MIRROR_X),
+            boolToUInt(gameConstants.PERLIN_MIRROR_Y),
             _listingId[0],
             _listingId[1],
             _listingId[2],
@@ -125,6 +148,9 @@ contract NightMarket is ReentrancyGuard {
         l.isActive = true;
 
         numListings++;
+
+        emit Listed();
+
         return (numListings - 1);
     }
 
