5522e6f0aa6ddf1420fb226198eb7e635ff472e6
nczhu <n@pragmatic.vc>
2022-04-15 18:26:21 +0100

    test: deployment should work

--- contracts/test/NightMarket.js
+++ contracts/test/NightMarket.js
@@ -1,12 +1,18 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const { smock } = require("@defi-wonderland/smock");
 
-let nmFactory;
-let nm;
-let lv;
-let sv;
-let game;
+const gameJSON = require("../../artifacts/contracts/darkforest/GetterInterface.sol/IGetter.json");
+const listJSON = require("../../artifacts/contracts/ListVerifier.sol/Verifier.json");
+const saleJSON = require("../../artifacts/contracts/SaleVerifier.sol/Verifier.json");
 
+// Contracts
+let nightmarket;
+let listVerifier;
+let saleVerifier;
+let fakeGame;
+
+// Accounts
 let seller;
 let buyer;
 let anyone;
@@ -17,33 +23,46 @@ let anyone;
 before(async function () {
 	[seller, buyer, anyone, ...addrs] = await ethers.getSigners();
 
-	const lvFactory = await ethers.getContractFactory("ListVerifier");
-	lv = await NightMarket.deploy();
+	const lvFactory = await ethers.getContractFactory("contracts/ListVerifier.sol:Verifier");
+	listVerifier = await lvFactory.deploy();
 
-	const svFactory = await ethers.getContractFactory("SaleVerifier");
-	sv = await NightMarket.deploy();
+	const svFactory = await ethers.getContractFactory("contracts/SaleVerifier.sol:Verifier");
+	saleVerifier = await svFactory.deploy();
 
-	const gameFactory = await ethers.getContractFactory("MockGameContract");
-	game = await NightMarket.deploy();
+	fakeGame = await smock.fake(gameJSON.abi);
+
+	// Stubs
+	fakeGame.getSnarkConstants.returns({
+		DISABLE_ZK_CHECKS: false,
+		PLANETHASH_KEY: 7,
+		SPACETYPE_KEY: 0,
+		BIOMEBASE_KEY: 8,
+		PERLIN_MIRROR_X: false,
+		PERLIN_MIRROR_Y: false,
+		PERLIN_LENGTH_SCALE: 0
+	});
 });
 
 /**
  * Deploys fresh contract for each unit test
  */
 beforeEach(async function () {
-
-	nmFactory = await ethers.getContractFactory("NightMarket");
-
-
-	nm = await NightMarket.deploy();
+	const nmFactory = await ethers.getContractFactory("NightMarket");
+	nightmarket = await nmFactory.deploy(listVerifier.address, saleVerifier.address, fakeGame.address);
 });
 
 
 describe("NightMarket contract", function () {
 	it("Deployment should work", async function () {
-		// TODO check snark constants
-		const snarkConstants = ;
-		expect(await nm.totalSupply()).to.equal(ownerBalance);
+		const zk = await nightmarket.zkConstants();
+		expect(zk.PLANETHASH_KEY.toNumber()).to.equal(7);
+		expect(zk.BIOMEBASE_KEY.toNumber()).to.equal(8);
+
+		const listAddress = await nightmarket.listVerifier();
+		expect(listAddress).to.equal(listVerifier.address);
+		const saleAddress = await nightmarket.saleVerifier();
+		expect(saleAddress).to.equal(saleVerifier.address);
+
 	});
 
 	it("List: Coordinate must be valid", async function () {
