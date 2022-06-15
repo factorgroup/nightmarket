d4c457cdcb407a9676b5907fc7b6afa74bf480b9
Pierre DM <pdaixmoreux@gmail.com>
2022-06-15 14:26:11 +0200

    feat: make transaction context available

--- client/plugin/components/MyTransactionContext.tsx
+++ client/plugin/components/MyTransactionContext.tsx
@@ -1,13 +1,24 @@
-import { h } from "preact";
+import { ComponentChildren, h } from "preact";
 import { createContext } from "preact";
-import { useState } from "preact/hooks";
+import { useEffect, useState } from "preact/hooks";
+import { useContract } from "../hooks/use-contract";
+import { useSigner } from "../hooks/use-signer";
+import { ActiveSigner } from "./SignerContext";
+import { Event as EthersEvent } from "ethers";
 
 // A session history of users on chain transactions
 // Can be of type `list` `ask` or `sale`
 export const MyTransactionContext = createContext([] as any);
 
-export const MyTransactionProvider = (props) => {
-	const [transactions, setTransactions] = useState([]);
+type MyTransactionProviderProps = {
+	children: ComponentChildren;
+	txs: EthersEvent[];
+};
+
+export const MyTransactionProvider = (props: MyTransactionProviderProps) => {
+	const [ transactions, setTransactions ] = useState<EthersEvent[]>(props.txs);
+	const { market } = useContract();
+	const signer = useSigner() as ActiveSigner;
 
 	return (
 		<MyTransactionContext.Provider
