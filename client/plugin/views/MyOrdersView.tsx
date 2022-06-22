a28108701ac80c886926030819381fc34a0f1a1a
Pierre DM <pdaixmoreux@gmail.com>
2022-06-22 18:28:00 +0200

    feat: start my orders view

--- client/plugin/views/MyOrdersView.tsx
+++ client/plugin/views/MyOrdersView.tsx
@@ -1,9 +1,18 @@
 import { h } from "preact";
+import { useState } from "preact/hooks";
+import { Listing, Order } from "../components/MyListingsContext";
+import { ActiveSigner } from "../components/SignerContext";
+import { getListingsWithOrdersFromAddress } from "../helpers/transactions";
+import { useListings } from "../hooks/use-listings";
+import { useSigner } from "../hooks/use-signer";
 
-export function MyOrdersView() {
+export function MyOrdersView () {
+	const signer = useSigner() as ActiveSigner;
+	const listings = useListings();
+	const [ myOrders, setMyOrders ] = useState<{ orders: Order[], listing: Listing; }[]>(getListingsWithOrdersFromAddress(listings, signer.address));
 	return (
 		<div>
 			My orders...
 		</div>
-	)
+	);
 }
