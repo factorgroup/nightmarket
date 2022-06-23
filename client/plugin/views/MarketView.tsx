c312aba6d9c602c101a2ccd8d0dde4254794a990
Pierre DM <pdaixmoreux@gmail.com>
2022-06-23 15:56:42 +0200

    chore: refacto market make order view using hooks

--- client/plugin/views/MarketView.tsx
+++ client/plugin/views/MarketView.tsx
@@ -1,20 +1,15 @@
-import { BigNumber, ethers } from "ethers";
+import { ethers, Transaction } from "ethers";
 import { FunctionalComponent, h } from "preact";
 import { useState } from "preact/hooks";
 import { Button } from "../components/Button";
 import { Listing } from "../components/MyListingsContext";
-import { getPublicKey } from "../helpers/pubKeyRetriever";
 import { useConnection } from "../hooks/use-connection";
 import { useListings } from "../hooks/use-listings";
 import { ListingGridHeaderRow, ListingItem, OrdersView } from "./MyListingsView";
-
-import { getPublicKeyAsPoint as getEDDSAPublicKey } from "../helpers/ed25519";
-
-// @ts-ignore
-import { Scalar, ZqField } from 'https://cdn.skypack.dev/ffjavascript-browser@0.0.3';
-import { mimcHash } from "@darkforest_eth/hashing";
+import { useRecoverPubKey } from "../hooks/use-recoverpubkey";
+import { useSharedKeyCommitment } from "../hooks/use-ecdh";
 import { useContract } from "../hooks/use-contract";
-const F = new ZqField(Scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495617"));
+
 
 type OrderItemProps = {
 	listing: Listing;
@@ -34,36 +29,20 @@ const orderStyles = { // TODO: unify all styles in single file
 	}
 };
 export const OrderView: FunctionalComponent<OrderItemProps> = (props) => {
-	const [ keyCommitment, setkeyCommitment ] = useState<BigNumber>();
-	const { market } = useContract();
-	const connection = useConnection();
-	const privateKey = connection.getPrivateKey();
-	const buyerSigningKey = new ethers.utils.SigningKey(privateKey); // TODO: refactor this in proper getSharedSecret() - see MyListingsView
-	const sellerPublicKey = getPublicKey(props.listing.tx); // get seller public key from listing tx
-	const sharedSecret = buyerSigningKey.computeSharedSecret(sellerPublicKey);
-	const sellerAddress = ethers.utils.computeAddress(sellerPublicKey);
-	
-
-	const makeOrder = async (keyCommitment: BigNumber) => {
-		console.log("ASK ORDER SHARED SECRET: ", sharedSecret, "SELLER ADDRESS", sellerAddress);
-		// const ask = await market.ask(props.listing.listingId, keyCommitment, { value: props.listing.price });
-
-	};
-
 	/**
 	 * TODO: should add a whole confirmation flow to the transaction.
 	 */
+	const { market } = useContract();
+	const privateKey = (useConnection()).getPrivateKey();
+	const buyerSigningKey = new ethers.utils.SigningKey(privateKey);
+	const { pubKey: sellerPublicKey, computedAddress: sellerComputedAddress } = useRecoverPubKey(props.listing.tx as Transaction);
+	const keyCommitment = useSharedKeyCommitment(buyerSigningKey, sellerPublicKey);
 
-	const calculateExpecteddKeyCommitment = async () => {
-		const sharedKey = await getEDDSAPublicKey(F.e(sharedSecret));
-		const sharedKeyCommitment = mimcHash(0)(F.e(sharedKey.x), F.e(sharedKey.y)).toString();
-		const expectedKeyCommitment = BigNumber.from(sharedKeyCommitment);
-		setkeyCommitment(expectedKeyCommitment);
-		const ask = await makeOrder(expectedKeyCommitment);
-		console.log("Ask placed");
+	const makeOrder = async () => {
+		console.log("ASK ORDER KEY COMMITMENT: ", keyCommitment, "SELLER ADDRESS", sellerComputedAddress);
+		const ask = await market.ask(props.listing.listingId, keyCommitment, { value: props.listing.price });
+
 	};
-
-
 
 	const keyCommitmentRowTitle = keyCommitment ? 'Shared key commitment' : '';
 
@@ -85,7 +64,7 @@ export const OrderView: FunctionalComponent<OrderItemProps> = (props) => {
 				<div style={orderStyles.longText}>{keyCommitment ? keyCommitment.toString() : ''}</div>
 			</div>
 			<div>
-				<Button disabled={!props.listing.isActive} children={('order')} style={{ width: "100%" }} onClick={async () => await calculateExpecteddKeyCommitment()} />
+				<Button disabled={!props.listing.isActive} children={('order')} style={{ width: "100%" }} onClick={async () => await makeOrder()} />
 			</div>
 		</div>
 	);
--- contracts/scripts/recoverPubKey.ts
+++ contracts/scripts/recoverPubKey.ts
@@ -1,39 +1,44 @@
-import Common, { Chain, Hardfork } from '@ethereumjs/common';
+import Common from '@ethereumjs/common';
 import { FeeMarketEIP1559Transaction } from '@ethereumjs/tx';
 import { Address, toChecksumAddress } from 'ethereumjs-util';
-
 import { ethers } from 'ethers';
 import * as hre from 'hardhat';
-const publicKeyToAddress = require('ethereum-public-key-to-address');
 
 async function main () {
+    const privateKeyHolder = hre.config.networks.hardhat.accounts[ 0 ].privateKey;
+    const privateSigningKey = new hre.ethers.utils.SigningKey(privateKeyHolder);
+
     const address = "0xfC5632d904f2B40A9D2E215E55bAd747b1F776C6";
     const nmFactory = await hre.ethers.getContractFactory("NightMarket");
     const nm = nmFactory.attach("0x6792e95058514c51aB07533B0bE4B3ADB6FFa1d8");
     const [ ask ] = await nm.queryFilter(nm.filters.Asked(address, 12));
     const tx = await ask.getTransaction();
+    // delete tx.gasPrice
     const txData = {
-        gasLimit: tx.gasLimit.toNumber(),
-        value: tx.value.toNumber(),
+        gasLimit: tx.gasLimit,
+        value: tx.value,
         nonce: tx.nonce,
         data: tx.data,
         chainId: tx.chainId,
         to: tx.to!,
         type: 2,
-        maxFeePerGas: tx.maxFeePerGas!.toNumber(),
-        maxPriorityFeePerGas: tx.maxPriorityFeePerGas!.toNumber(),
-        s: tx.s!,
-        r: tx.r!,
-        v: tx.v!
+        maxFeePerGas: tx.maxFeePerGas!,
+        maxPriorityFeePerGas: tx.maxPriorityFeePerGas!,
     };
+    const rsTx = await ethers.utils.resolveProperties(txData)
+    const raw = ethers.utils.serializeTransaction(rsTx)
+    const msgHash = ethers.utils.keccak256(raw) // as specified by ECDSA
+    const msgBytes = ethers.utils.arrayify(msgHash)
+    const signature = ethers.utils.joinSignature({r: tx.r!, s: tx.s!, v: tx.v!})
+    const pubkey = hre.ethers.utils.recoverPublicKey(msgBytes, signature);
+    const computedAddress = hre.ethers.utils.computeAddress(pubkey);
 
-    const common = Common.custom({ chainId: 100 });
-    common[ "eips" ] = () => [ 1559 ];
-    const t = FeeMarketEIP1559Transaction.fromTxData(txData, { common });
-    const pubKey = t.getSenderPublicKey();
-    const computedAddress = Address.fromPublicKey(pubKey)
 
-    console.log(address, toChecksumAddress(computedAddress.toString()))
+    console.log({
+        initialAddress: address,
+        pubKey: pubkey,
+        computedAddress
+    });
 
 }
 
