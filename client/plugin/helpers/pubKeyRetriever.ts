58a87b99abd2670864d861aaabcf01db789909e9
Pierre DM <pdaixmoreux@gmail.com>
2022-06-23 15:55:07 +0200

    chore: public key retriever from transaction

--- client/plugin/helpers/pubKeyRetriever.ts
+++ client/plugin/helpers/pubKeyRetriever.ts
@@ -1,52 +1,35 @@
+import { ethers, Transaction } from 'ethers';
+
+
 /**
  * Util for retrieving the public key associated to an eth address
  */
-import { utils } from 'ethers';
 
-function getRawTransaction (tx) {
 
-    // Change relevant fields for EIP 1559
-    const txFields = {
+
+export async function getPublicKey (tx: ethers.providers.TransactionResponse | ethers.Transaction) {
+    const txData = {
         gasLimit: tx.gasLimit,
         value: tx.value,
         nonce: tx.nonce,
         data: tx.data,
         chainId: tx.chainId,
-        to: tx.to,
+        to: tx.to!,
         type: 2,
-        maxFeePerGas: tx.maxFeePerGas,
-        maxPriorityFeePerGas: tx.maxPriorityFeePerGas
-    } 
-
-    const sigFields = {
-        v: tx.v,
-        r: tx.r,
-        s: tx.s
-    }
-    // Seriailze the signed transaction
-    const raw = utils.serializeTransaction(txFields, sigFields);
-
-    // Double check things went well
-    if (utils.keccak256(raw) !== tx.hash) { throw new Error("serializing failed!"); }
-
-    return raw;
-}
-
-function getExpandedSignature (tx) {
-    return {
-        r: tx.r,
-        s: tx.s,
-        v: tx.v
+        maxFeePerGas: tx.maxFeePerGas!,
+        maxPriorityFeePerGas: tx.maxPriorityFeePerGas!,
     };
-}
+    const rsTx = await ethers.utils.resolveProperties(txData);
+    const raw = ethers.utils.serializeTransaction(rsTx);
+    const msgHash = ethers.utils.keccak256(raw); // as specified by ECDSA
+    const msgBytes = ethers.utils.arrayify(msgHash);
+    const signature = ethers.utils.joinSignature({ r: tx.r!, s: tx.s!, v: tx.v! });
+    const pubKey = ethers.utils.recoverPublicKey(msgBytes, signature);
+    const computedAddress = ethers.utils.computeAddress(pubKey);
 
-export function getPublicKey (tx) {
-    const rawTx = getRawTransaction(tx);
-    const msgHash = utils.keccak256(rawTx);
-    const msgBytes = utils.arrayify(msgHash);
-    const expandedSig = getExpandedSignature(tx);
-    const signature = utils.joinSignature(expandedSig);
-    const recoveredPubKey = utils.recoverPublicKey(msgBytes, signature);
-    return recoveredPubKey;
+    if (computedAddress != tx.from) {
+        throw (`Error in computed address from public key: ${computedAddress} != ${tx.from}`);
+    }
+    return { pubKey, computedAddress };
 }
 
